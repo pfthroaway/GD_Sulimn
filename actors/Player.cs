03b73bbeec346b3dafc4c85420bdac6b0973ea1a
pfthroaway <pfthroaway@mail.com>
2019-11-12 23:13:27 -0600

    Allow player to move multiple tiles by holding a direction.

--- actors/Player.cs
+++ actors/Player.cs
@@ -6,6 +6,7 @@ public class Player : Area2D
     private readonly int TileSize = 64;
     public bool Disabled;
     private RayCast2D ray;
+    private float moveDelay;
 
     private readonly Dictionary<string, Vector2> inputs = new Dictionary<string, Vector2> {
         { "up", Vector2.Up },
@@ -22,9 +23,6 @@ public class Player : Area2D
 
     public override void _Input(InputEvent @event)
     {
-        foreach (string dir in inputs.Keys)
-            if (!Disabled && @event.IsActionPressed(dir))
-                Move(dir);
     }
 
     public void Move(string dir)
@@ -33,13 +31,17 @@ public class Player : Area2D
         ray.ForceRaycastUpdate();
         if (!ray.IsColliding())
             Position += inputs[dir] * TileSize;
+        moveDelay = 0;
     }
 
     public void DisablePlayer() => Disabled = true;
 
-    //  // Called every frame. 'delta' is the elapsed time since the previous frame.
-    //  public override void _Process(float delta)
-    //  {
-    //
-    //  }
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(float delta)
+    {
+        moveDelay += delta;
+        foreach (string dir in inputs.Keys)
+            if (!Disabled && Input.IsActionPressed(dir) && moveDelay >= 0.15)
+                Move(dir);
+    }
 }
