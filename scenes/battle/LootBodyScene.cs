6188123748bf1fe540e95ebcc230a6d44ab84fbe
pfthroaway <pfthroaway@mail.com>
2019-11-25 16:36:16 -0600

    Allowed for looting of gold from a dead body.
    I also added a couple more textures.

--- scenes/battle/LootBodyScene.cs
+++ scenes/battle/LootBodyScene.cs
@@ -8,8 +8,28 @@ namespace Sulimn.Scenes.Battle
     {
         private GridEquipment HeroEquipment, EnemyEquipment;
         private GridInventory HeroInventory, EnemyInventory;
+        private Button BtnLootGold;
+        private Label LblGold;
+
+        #region Button Click
+
+        private void _on_BtnLootGold_pressed()
+        {
+            GameState.CurrentHero.Gold += GameState.CurrentEnemy.Gold;
+            GameState.CurrentEnemy.Gold = 0;
+            GameState.UpdateDisplay = true;
+            ToggleLootGold();
+        }
 
         private void _on_BtnReturn_pressed() => GetTree().ChangeSceneTo(GameState.GoBack());
+
+        #endregion Button Click
+
+        private void ToggleLootGold()
+        {
+            LblGold.Text = GameState.CurrentEnemy.GoldToStringWithText;
+            BtnLootGold.Disabled = GameState.CurrentEnemy.Gold == 0;
+        }
 
         #region Save
 
@@ -33,17 +53,25 @@ namespace Sulimn.Scenes.Battle
 
         #endregion Save
 
-        // Called when the node enters the scene tree for the first time.
-        public override void _Ready()
+        private void AssignControls()
         {
             HeroInventory = (GridInventory)GetNode("HeroInventory");
             EnemyInventory = (GridInventory)GetNode("EnemyInventory");
             HeroEquipment = (GridEquipment)GetNode("HeroEquipment");
             EnemyEquipment = (GridEquipment)GetNode("EnemyEquipment");
+            BtnLootGold = (Button)FindNode("BtnLootGold");
+            LblGold = (Label)FindNode("LblGold");
+        }
+
+        // Called when the node enters the scene tree for the first time.
+        public override void _Ready()
+        {
+            AssignControls();
             HeroInventory.SetUpInventory(GameState.CurrentHero.Inventory);
             EnemyInventory.SetUpInventory(GameState.CurrentEnemy.Inventory, true);
             HeroEquipment.SetUpEquipment(GameState.CurrentHero.Equipment, GameState.CurrentHero.Level, GameState.CurrentHero.Class);
             EnemyEquipment.SetUpEquipment(GameState.CurrentEnemy.Equipment, 0, null, true);
+            ToggleLootGold();
         }
 
         // Called every frame. 'delta' is the elapsed time since the previous frame.
